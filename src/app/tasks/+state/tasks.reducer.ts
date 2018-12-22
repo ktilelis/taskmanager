f4a5381445bc5c8c1af19b0ec26ac677dd41fc13
Kostas Tilelis <kostas.tilelis@gmail.com>
2018-12-22 23:44:48 +0200

    Update task selectors

--- src/app/tasks/+state/tasks.reducer.ts
+++ src/app/tasks/+state/tasks.reducer.ts
@@ -1,5 +1,6 @@
-import { Task, TasksState } from './tasks.interfaces';
+import { Task, TasksState, TaskStatus } from './tasks.interfaces';
 import { TasksActions, TasksActionType } from './tasks.actions';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 export function reducer(state: Task[], action: TasksActions): Task[] {
     switch (action.type) {
@@ -11,19 +12,29 @@ export function reducer(state: Task[], action: TasksActions): Task[] {
         case TasksActionType.REMOVE_TASK:
             return [ ...state.filter(s => s.id !== action.taskId) ];
         case TasksActionType.UPDATE_STATUS: {
-            const _state = [...state];
+            const _state = [ ...state ];
 
             const taskIndex = state.findIndex(s => s.id === action.taskId);
-            const _task = { ..._state[ taskIndex] };
+            const _task = { ..._state[ taskIndex ] };
             _task.status = action.status;
             _task.updatedOn = new Date();
 
             _state.splice(taskIndex, 1, _task);
             return _state;
         }
+        case TasksActionType.TOGGLE_ALL_TASKS_STATUS: {
+            const status: TaskStatus = action.selectAll ? 'COMPLETED' : 'PENDING';
+            return state.map(t => ({ ...t, status }));
+        }
         default:
             return state;
     }
 }
 
-export const selectTasks = (state: TasksState) => state.tasks;
+// export const selectTasks = (state: TasksState) => state.tasks;
+export const selectTasksState = createFeatureSelector<Task[]>('tasks');
+export const selectTasks = createSelector(selectTasksState, (state: Task[]) => state);
+export const selectTasksByStatus = (status: TaskStatus) => createSelector(selectTasks, tasks => tasks.filter(t => t.status === status));
+export const selectCompletedTasks = createSelector(selectTasks, s => s.filter(t => t.status === 'COMPLETED'));
+export const selectPendingTasks = createSelector(selectTasks, s => s.filter(t => t.status === 'PENDING'));
+export const areAllCompleted = createSelector(selectTasks, s => s.length && s.every(t => t.status === 'COMPLETED'));
--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -1,12 +1,14 @@
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { takeUntil } from 'rxjs/operators/takeUntil';
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 
 import { generateUuid } from '@shared/uuid-generator';
 
 import { Task } from '../+state/tasks.interfaces';
-import { AddTask, RemoveTask, UpdateTaskStatus } from '../+state/tasks.actions';
+import { AddTask, RemoveTask, ToggleAllTasksStatus, UpdateTaskStatus } from '../+state/tasks.actions';
 import * as fromTasks from '../+state/tasks.reducer';
 
 @Component({
@@ -14,16 +16,30 @@ import * as fromTasks from '../+state/tasks.reducer';
     templateUrl: './task-list.component.html',
     styleUrls: [ './task-list.component.scss' ]
 })
-export class TaskListComponent implements OnInit {
+export class TaskListComponent implements OnInit, OnDestroy {
+
+    unsubscribe$ = new Subject<void>();
 
     tasks$: Observable<Task[]>;
+    completedTasks$: Observable<Task[]>;
+    pendingTasks$: Observable<Task[]>;
+
     taskDescription: string;
+    toggleAllStatus = false;
 
     constructor(private store: Store<any>) {
     }
 
     ngOnInit() {
         this.tasks$ = this.store.pipe(select(fromTasks.selectTasks));
+        this.completedTasks$ = this.store.pipe(select(fromTasks.selectTasksByStatus('COMPLETED')));
+        this.pendingTasks$ = this.store.pipe(select(fromTasks.selectTasksByStatus('PENDING')));
+        this.store.pipe(
+            select(fromTasks.areAllCompleted),
+            takeUntil(this.unsubscribe$)
+        ).subscribe(allCompleted => this.toggleAllStatus = allCompleted);
+
+
     }
 
     addTask() {
@@ -51,6 +67,12 @@ export class TaskListComponent implements OnInit {
     }
 
     toggleSelectAll() {
+        this.toggleAllStatus = !this.toggleAllStatus;
+        this.store.dispatch(new ToggleAllTasksStatus(this.toggleAllStatus));
+    }
 
+    ngOnDestroy() {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
     }
 }
